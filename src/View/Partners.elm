49f88e7e306a433ea6650ea346f038db02492c58
dylandoamaral <do.amaral.dylan@gmail.com>
2022-08-09 15:09:52 +0100

    fix: partners page homogeneity

--- src/View/Partners.elm
+++ src/View/Partners.elm
@@ -2,20 +2,19 @@ module View.Partners exposing (view)
 
 import Components.DataStruct as SponsorKind exposing (GlobalData, SponsorKind)
 import Components.Utils exposing (transform)
-import Html exposing (Html, div, h1, text)
+import Html exposing (Html, div, h2, text)
 import Html.Attributes as Attr exposing (class)
 
 
 view : GlobalData -> Html msg
 view gd =
-    div [ Attr.class "flex  flex-col gap-y-20" ]
-        [ div [ Attr.class "flex justify-center" ] [ h1 [] [ text "Platinum Sponsor" ] ]
+    div [ Attr.class "flex  flex-col gap-y-20 partners" ]
+        [ div [ Attr.class "flex justify-center" ] [ h2 [] [ text "Platinum Sponsor" ] ]
         , div [ class "flex justify-around" ] (transform gd.sponsors.platine SponsorKind.Platine)
-        , div [ Attr.class "flex justify-center" ] [ h1 [] [ text "Gold Sponsor" ] ]
+        , div [ Attr.class "flex justify-center" ] [ h2 [] [ text "Gold Sponsor" ] ]
         , div [ class "flex justify-around" ] (transform gd.sponsors.gold SponsorKind.Gold)
-        , div [ Attr.class "flex justify-center" ] [ h1 [] [ text "Silver Sponsor" ] ]
+        , div [ Attr.class "flex justify-center" ] [ h2 [] [ text "Silver Sponsor" ] ]
         , div [ class "flex justify-around" ] (transform gd.sponsors.silver SponsorKind.Silver)
-        , div [ Attr.class "flex justify-center" ] [ h1 [] [ text "J'aime Scala Sponsor" ] ]
+        , div [ Attr.class "flex justify-center" ] [ h2 [] [ text "J'aime Scala Sponsor" ] ]
         , div [ class "flex justify-around" ] (transform gd.sponsors.jaimeScala SponsorKind.JaimeScala)
-        , Html.hr [] []
         ]
