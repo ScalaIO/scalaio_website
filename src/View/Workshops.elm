f1261c8814bf12b5573ab1bb70dec9707f4bb41c
Jonathan Winandy <jonathan.winandy@gmail.com>
2022-10-20 19:32:28 +0200

    smalls changes

--- src/View/Workshops.elm
+++ src/View/Workshops.elm
@@ -2,7 +2,7 @@ module View.Workshops exposing (..)
 
 import Components.DataStruct exposing (GlobalData)
 import Components.Utils exposing (transformWorkshop)
-import Html exposing (Html, a, div, h2, text)
+import Html exposing (Html, a, div, h2, text, br, b)
 import Html.Attributes as Attr exposing (class)
 
 
@@ -12,13 +12,17 @@ view gd =
         [ h2 [ class "workshop-program-title" ]
             [ text "Workshop 2022 Program" ]
         , div [ class "workshop-info" ]
-            [ div [] [ text (String.append "Workshop venue address : " gd.ws.address) ] ]
+            [ b [] [text "Workshop venue address : "] , text gd.ws.address ]
         , div [ class "workshop-info" ]
-            [ div [] [ text (String.append "Workshop schedule : " gd.ws.horaires) ]
-            ]
+            [ b [] [text  "Workshop schedule : "] , text gd.ws.horaires ]
+        , div [class "workshop-info"]
+            [ b [] [text "Scala Spree : "] , text "Thurday, Novembre 3rd. From 1:30 pm to 5h30 pm, same venue."
+            , br [] []
+            , text "More informations for the opensource spreee "
+            , a [Attr.href "https://github.com/scalacenter/sprees#paris-france-thursday-3rd-november-2022"] [text "Here."]]
         , div [ class "workshop-info workshop-info-warning" ]
             [ div []
-                [ text "Workshop ticket included pass conference. It can be bought from our "
+                [ text "Workshop ticket include pass conference. It can be bought from our "
                 , a [ Attr.href "/" ] [ text "ticketing" ]
                 ]
             ]
