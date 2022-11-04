27e8ec5b4c6ee89568377ca3d66f68e8061e1bfb
dylandoamaral <do.amaral.dylan@gmail.com>
2022-11-04 20:20:21 +0100

    feat: add homepage goodbye message

--- src/View/Home.elm
+++ src/View/Home.elm
@@ -18,23 +18,4 @@ cfp =
 view : Html msg
 view =
     div [ Attr.class "home" ]
-        [h2 [ Attr.class "home-title" ] [ text "Buy your ticket for edition 2022 now" ]
-        , p [] [ text "ScalaIO is a community based, non-profit event. See our ", a [ Attr.href "/faq", Attr.target "_blank" ] [ text "F.A.Q" ], text " for more information." ]
-        , iframe
-            [ Attr.src "https://yurplan.com/event/Scala-IO-2022/88496/tickets/widget?widget=dGlja2V0aW5nV2lkZ2V0WXBfODY1MzdfODk1MDQ%3D&from=widget_89504&wversion=1&culture=fr"
-            , class "home-ticketing"
-            ]
-            []
-        , node "script"
-            [ Attr.type_ "text/javascript"
-            , Attr.src "https://assets.yurplan.com/yurplan-v1/dist/widget.js"
-            ]
-            []
-        , div [ class "home-ticketing-disclaimer" ]
-            [ text "If you get an error buying through the Iframe, please try using the  "
-            , a [ Attr.href "https://yurplan.com/event/Scala-IO-2022/88496", Attr.target "_blank", class "href-link" ] [ text " minisite" ]
-            , text " and "
-            , a [ Attr.href "mailto:contact@scala.io", Attr.target "_blank", class "href-link" ] [ text "let us know" ]
-            , text "."
-            ]
-        ]
+        [ p [ Attr.class "home-title" ] [ text "Scala.io 2022 is already over 🤧" ], p [ Attr.class "home-title" ] [ text "Thank you for coming and see you the 26th and 27th of October 2023 for the 10th edition !" ] ]
